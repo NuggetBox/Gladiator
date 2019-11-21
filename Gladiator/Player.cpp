b9f7f64215d109c87abf825a51a3011ec0f048c6
Alvin Andersson Eriksson <alvin.anderssonerik@learnet.se>
2019-11-21 09:44:24 +0100

    Player

--- Gladiator/Player.cpp
+++ Gladiator/Player.cpp
@@ -1,1 +1,52 @@
 #include "Player.h"
+
+Player::Player()
+{
+
+
+}
+
+
+Player::~Player()
+{
+}
+
+void Player::Init(const sf::Texture& aTexture)
+{
+	mySprite.setTexture(aTexture);									//Här sätter vi texturen till vår Sprite
+	//mySprite.setTextureRect(sf::IntRect(0, 0, 64, 64));			//Denna används om vi endast vill visa en del av texturen, eller om vi t.ex. vill animera den
+	mySprite.setColor(sf::Color(255, 255, 255, 200));				//Här sätter vi färgen
+	mySprite.setPosition(100, 25);									//Och här sätter vi positionen
+
+	mySpeed = 250;
+}
+
+bool Player::Update(const float& someDelta)							//Delta Time kommer in från App-klassen.
+{
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))				//Här kollar vi spelarens input, om spelaren trycker A så kommer vi flytta vår sprite i X-led.
+	{
+		mySprite.move(-mySpeed * someDelta, 0);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
+		mySprite.move(mySpeed * someDelta, 0);
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+	{
+		mySprite.move(0, -mySpeed * someDelta);						//Och här flyttar vi i Y-led istället.
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	{
+		mySprite.move(0, mySpeed * someDelta);
+	}
+
+
+	return true;
+}
+
+void Player::Draw(sf::RenderWindow& aWindow)
+{
+	aWindow.draw(mySprite);											//Här ritar vi ut vår sprite, notera att det är en referens som vi tar emot.
+}
--- Gladiator/Player.h
+++ Gladiator/Player.h
@@ -1,6 +1,21 @@
-#pragma once
+#ifndef PLAYER_HEADER
+#define PLAYER_HEADER
+
+#include <SFML/Graphics.hpp>
 
 class Player
 {
+public:
+	Player();
+	~Player();
 
+	void Init(const sf::Texture& aTexture);
+
+	bool Update(const float& someDelta);
+	void Draw(sf::RenderWindow& aWindow);
+private:
+	sf::Sprite mySprite;
+	float mySpeed;
 };
+
+#endif
