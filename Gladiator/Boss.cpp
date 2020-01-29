7e74addbd95d9b91b5260456b6b466c55b066a0d
Alvin Andersson Eriksson <alvin.anderssonerik@learnet.se>
2020-01-29 09:24:59 +0100

    changes

--- Gladiator/Boss.cpp
+++ Gladiator/Boss.cpp
@@ -3,26 +3,13 @@
 
 Boss::Boss()
 {
-	player = gameInfo::getPlayerPointer();
 }
 
 void Boss::Update(const float& someDelta)
 {
-	//if (player->GetPosition(). )
-	//{
-
-	//}
-}
-
-void Boss::Attack()
-{
 
 }
 
-void Boss::Idle()
-{
-
-}
 
 void Boss::Ultimate()
 {
---
+++ Gladiator/StartingBoss.cpp
@@ -0,0 +1,45 @@
+#include "StartingBoss.h"
+
+StartingBoss::StartingBoss()
+{
+	myPlayer = gameInfo::getPlayerPointer();
+}
+
+void StartingBoss::Update(const float& someDelta)
+{
+	if (myPlayer->GetPosition().Distance(myPosition) < 100)
+	{
+		Attack(true);
+	}
+	if (myPlayer->GetPosition().Distance(myPosition) > 100)
+	{
+		Attack(false);
+	}
+}
+
+void StartingBoss::Attack(bool aNear)
+{
+	switch (aNear)
+	{
+	case 0:
+		
+		break;
+	case 1:
+		break;
+	}
+}
+
+void StartingBoss::Ultimate()
+{
+
+}
+
+void StartingBoss::Idle()
+{
+
+}
+
+StartingBoss::~StartingBoss()
+{
+
+}
